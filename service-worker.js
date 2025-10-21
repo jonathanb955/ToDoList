4ccb62a283b49fac8329bf5652d2210faf15c56d
Jonathan <baojonathan95@gmail.com>
2025-10-21 02:50:46 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'todolist-cache-v1';
+const urlsToCache = [
+    './',
+    './index.html',
+    './manifest.json',
+    './favicon.ico'
+];
+
+// Installation du SW
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+// Activation et nettoyage ancien cache
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+        )
+    );
+});
+
+// Interception des requêtes
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
+});
